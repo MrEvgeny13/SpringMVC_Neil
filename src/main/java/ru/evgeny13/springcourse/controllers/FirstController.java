934fd63eb4ee2200c9f83455980f3e403b482e39
Evgeny Rudoy <rudoy95@mail.ru>
2021-03-22 22:22:43 +0300

    Added @RequestParam annotations

---
+++ src/main/java/ru/evgeny13/springcourse/controllers/FirstController.java
@@ -0,0 +1,27 @@
+package ru.evgeny13.springcourse.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import javax.servlet.http.HttpServletRequest;
+
+@Controller
+@RequestMapping("/first")
+public class FirstController {
+
+    @GetMapping("/hello")
+    public String helloPage(@RequestParam(value = "name", required = false) String name,
+                            @RequestParam(value = "surname", required = false) String surname) {
+
+        System.out.println("Hello, " + name + " " + surname);
+
+        return "first/hello";
+    }
+
+    @GetMapping("/goodbye")
+    public String goodByePage() {
+        return "first/goodbye";
+    }
+}
